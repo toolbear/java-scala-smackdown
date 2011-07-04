9fa154e12ff1b15bfe946cc0eebf1775b0be00e2
Tim Taylor <tim@tool-man.org>
2011-07-04 16:24:20 -0400

    Round 1, java versus scala smackdown

---
+++ src/test/java/smackdown/redcorner/Round01Test.java
@@ -0,0 +1,13 @@
+package smackdown.redcorner;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+public class Round01Test {
+
+    @Test public void trueIsTrue() {
+        assertThat(true, is(true));
+    }
+
+}
---
+++ src/test/scala/smackdown/bluecorner/Round01Test.scala
@@ -0,0 +1,13 @@
+package smackdown
+
+import org.scalatest.Spec
+import org.scalatest.matchers.ShouldMatchers
+
+
+class Round01ScalaTest extends Spec with ShouldMatchers {
+
+    it("speaks the truth") {
+      true should be (true)
+    }
+
+}
