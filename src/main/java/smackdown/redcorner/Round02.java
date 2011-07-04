d0f3bd529f4238fd3df028415aa3960ac5771eb0
Tim Taylor <tim@tool-man.org>
2011-07-04 16:26:36 -0400

    round 2, hello world

---
+++ src/main/java/smackdown/redcorner/Round02.java
@@ -0,0 +1,7 @@
+package smackdown.redcorner;
+
+public class Round02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/main/scala/smackdown/bluecorner/Round02.scala
@@ -0,0 +1,36 @@
+package smackdown.bluecorner
+
+object Round02a {
+  def main(args: Array[String]) {
+    System.out.println("Hello Java-like World")
+  }
+}
+
+// ↓ page down ↓
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+object Round02b extends App {
+  println("Hello Idiomatic World")
+}
